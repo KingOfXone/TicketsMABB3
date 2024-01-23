151f6ff64bd5aa5845f4bacbedb49f42a0931acf
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-01-23 18:05:36 -0400

    Creando El Modelo

---
+++ Models/Tickets.cs
@@ -0,0 +1,34 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace TicketsMABB3.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Es necesario completar el campo Fecha")]
+        public DateTime Fecha { get; set; } = DateTime.Now; // Fecha del ticket
+
+        [Required(ErrorMessage = "Es necesario completar el campo ClienteId")]
+        [Range(1, int.MaxValue, ErrorMessage = "El ClienteId debe ser mayor que cero")]
+        public int ClienteId { get; set; } // ID del cliente
+
+        [Required(ErrorMessage = "Es necesario completar el campo SistemaId")]
+        [Range(1, int.MaxValue, ErrorMessage = "El SistemaId debe ser mayor que cero")]
+        public int SistemaId { get; set; } // ID del sistema
+
+        [Required(ErrorMessage = "Es necesario completar el campo PrioridadId")]
+        [Range(1, int.MaxValue, ErrorMessage = "El PrioridadId debe ser mayor que cero")]
+        public int PrioridadId { get; set; } // ID de prioridad
+
+        [Required(ErrorMessage = "Es necesario completar el campo SolicitadoPor")]
+        public string SolicitadoPor { get; set; } // Persona que solicita
+
+        [Required(ErrorMessage = "Es necesario completar el campo Asunto")]
+        public string Asunto { get; set; } // Asunto del ticket
+
+        [Required(ErrorMessage = "Es necesario completar el campo Descripcion")]
+        public string Descripcion { get; set; } // Descripción del ticket
+    }
+}
