cfee6e3ef7a19e601932f9340d04c246ab935bab
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-01-23 18:03:28 -0400

    Creando El Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketsMABB3.DAL
+{
+    public class Contexto :DbContext
+    {
+        public DbSet<Models.Tickets> Tickets { get; set; }
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+    }
+}
